90080a034cefc7b56ef08bdf597c872cd10080e6
Omid Golshan <omid.gt21@gmail.com>
2025-01-24 23:06:46 +0100

    🧹

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,3 @@
-import { Dataset } from './dataset';
-import { ScatterPlot } from './scatter-plot';
-
 export * from './dataset';
 export * from './line-chart';
 export * from './scatter-plot';
-
-const scatterPlot = new ScatterPlot();
-const dataset = new Dataset([
-  [1, 2],
-  [2, 3],
-  [3, 4],
-  [4, 5],
-  [5, 6],
-]);
-
-scatterPlot.plot(dataset, [1, 1, 2, 1, 1, 2]);
